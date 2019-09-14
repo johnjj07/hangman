3f58692c890c5a684ac3bf89106b8bf57630bf78
Jared Johnson <slurmsy@pop-os.localdomain>
2019-09-14 01:01:34 -0400

    Fix unit test

--- hangman/test/hangman_game_test.exs
+++ hangman/test/hangman_game_test.exs
@@ -14,7 +14,7 @@ defmodule Hangman.GameTest do
   test "state isn't changed for :won or :lost game" do
     for state <- [:won, :lost] do
       game = Game.new_game()
-             |> Map.put(:game_state, :won)
+             |> Map.put(:game_state, state)
       assert { ^game, _ } = Game.make_move(game, "x")
     end
   end
