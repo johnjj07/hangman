495565569275ebc4186d419e57832446e218f9e0
Jared Johnson <slurmsy@pop-os.localdomain>
2019-10-20 01:05:33 -0400

    Wrap up dictionary and start CLI

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -3,4 +3,9 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game(), to: Game
+  def make_move(game, guess) do
+    game = Game.make_move(game, guess)
+    {game, tally(game)}
+  end
+  defdelegate tally(game), to: Game
 end
