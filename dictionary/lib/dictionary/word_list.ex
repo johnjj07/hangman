f4fb8834f22fb0de6076d170ad5fbb070da07163
Jared Johnson <slurmsy@pop-os.localdomain>
2019-10-26 02:57:55 -0400

    Refactor dictionary API

---
+++ dictionary/lib/dictionary/word_list.ex
@@ -0,0 +1,14 @@
+defmodule Dictionary.WordList do
+
+  def start() do
+    "../../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+  def random_word(word_list) do
+    Enum.random(word_list)
+  end
+
+end
